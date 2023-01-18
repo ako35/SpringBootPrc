af9cbd045becc1e6b5e3de9866bbcb6e42b79123
Ali KOÇAN <alikcn35@gmail.com>
2023-01-18 12:40:49 +0300

    11820231240

---
+++ src/main/java/com/tpe/SpringBootPrc2Application.java
@@ -0,0 +1,13 @@
+package com.tpe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPrc2Application.class, args);
+	}
+
+}
